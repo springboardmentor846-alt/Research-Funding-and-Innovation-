25a8950a588726487c8677479b911796b1902baa
Ishitha06 <ishitha.hubli@gmail.com>
2026-08-06 19:13:41 +0530

    Fixed CSS

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,23 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import "./App.css";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./index.css";
+import "./App.css";
+
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
