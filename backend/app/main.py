242efee925d9073836f2e37a1876a780500d1881
Upendra Kumar <upendra80026@gmail.com>
2026-08-19 13:58:12 +0530

    Add full Docker containerization: backend Dockerfile, frontend Dockerfile with nginx, updated docker-compose with all 3 services

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
